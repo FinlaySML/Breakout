14d5b21c0f2f69e56a79213987c24680b5a7b0c8
FinlaySML <2100719@uad.ac.uk>
2025-10-18 14:25:56 +0100

    Enable anti-aliasing

--- Breakout/main.cpp
+++ Breakout/main.cpp
@@ -4,7 +4,9 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "Breakout");
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Breakout", sf::Style::Default, settings);
     GameManager gameManager{};
     gameManager.initialize();
 
